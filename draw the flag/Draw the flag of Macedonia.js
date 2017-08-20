445eb61b4e9371244ca3cc205e3d588ad3858092
Xemsssa <Xemsss@gmail.com>
2017-08-20 19:21:44 +0300

    draw the flag

---
+++ draw the flag/Draw the flag of Macedonia.js
@@ -0,0 +1,42 @@
+// Draw the flag of Macedonia
+
+var rect = new Path.Rectangle(0, 0, 360, 240);
+rect.fillColor = 'red';
+ 
+var ray1 = new Path();
+ray1.add(0, 240);
+ray1.add(60, 240);
+ray1.add(300, 0);
+ray1.add(360, 0);
+ray1.fillColor = 'yellow';
+
+var ray2 = new Path();
+ray2.add(0, 100);
+ray2.add(0, 140);
+ray2.add(360, 100);
+ray2.add(360, 140);
+ray2.fillColor = 'yellow';
+
+var ray1 = new Path();
+ray1.add(0, 0);
+ray1.add(60, 0);
+ray1.add(300, 240);
+ray1.add(360, 240);
+ray1.fillColor = 'yellow';
+
+var ray2 = new Path();
+ray2.add(160, 0);
+ray2.add(200, 0);
+ray2.add(160, 240);
+ray2.add(200, 240);
+ray2.fillColor = 'yellow';
+
+
+
+var circle1 = new Path.Circle(180, 120, 40);
+circle1.fillColor = 'red';
+
+
+var circle1 = new Path.Circle(180, 120, 35);
+circle1.fillColor = 'yellow';
+
